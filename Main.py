6f67bc9cfa5bb18af7540cac488ab3ab0779f721
AlexisDeLaCurzHernandez <alexisdelacruzhernandez@gmail.com>
2024-05-07 16:12:09 -0300

    Small change

--- Main.py
+++ Main.py
@@ -2,12 +2,13 @@ from PyQt6.QtWidgets import QApplication
 from Window import MainWindow
 
 
-# Create app
-App = QApplication([])
+if __name__ == "__main__":
+    # Create app
+    App = QApplication([])
 
-# Create and show main window
-Window = MainWindow()
-Window.show()
+    # Create and show main window
+    Window = MainWindow()
+    Window.show()
 
-# App execution
-App.exec()
+    # App execution
+    App.exec()
