93440614c0a6c444c84f4d87adc3b945647761d8
AlexisDeLaCurzHernandez <alexisdelacruzhernandez@gmail.com>
2024-05-07 15:21:27 -0300

    Delete button

--- Config_widgets.py
+++ Config_widgets.py
@@ -49,10 +49,10 @@ class Label(QLabel):
 
 # Modified line edit
 class LineEdit(QLineEdit):
-    def __init__(self, text):
+    def __init__(self, text, max_length):
         super().__init__()
         self.setPlaceholderText(text)
-        self.setMaxLength(13)
+        self.setMaxLength(max_length)
         self.setFixedSize(120, 35)
         self.setStyleSheet(
             "LineEdit{"
@@ -69,8 +69,8 @@ class LineEdit(QLineEdit):
 class Graphic(FigureCanvasQTAgg):
     def __init__(self, width=4, height=3, dpi=100):
         self.colors = {
-            "Current": "g",
-            "Voltage": "r",
+            "Current": "#51CCFE",
+            "Voltage": "#51FE6B",
             "Default": principal_color,
         }
         self.axis_limit = 0
@@ -95,39 +95,47 @@ class Graphic(FigureCanvasQTAgg):
         y_value = module * sin(angle)
 
         # Detect the type of the label and transform to LaTeX
+        label = label[1:]
         color = self.colors.get("Default")
-        if label[0] == "I" or label == "i":
-            color = self.colors.get("Current")
-            label = "$" + label[0] + "_{" + label[1:] + "}$"
-        elif label[0] == "V" or label[0] == "v":
-            color = self.colors.get("Voltage")
-            label = "$" + label[0] + "_{" + label[1:] + "}$"
-        else:
-            label = "$" + label + "$"
+        if label:
+            if label[0] == "I" or label == "i":
+                color = self.colors.get("Current")
+                label = "$" + label[0] + "_{" + label[1:] + "}$"
+            elif label[0] == "V" or label[0] == "v":
+                color = self.colors.get("Voltage")
+                label = "$" + label[0] + "_{" + label[1:] + "}$"
+            else:
+                label = "$" + label + "$"
 
         # Setting the axis limit
         if abs(x_value) > self.axis_limit:
             self.axis_limit = abs(x_value)
         elif abs(y_value) > self.axis_limit:
             self.axis_limit = abs(y_value)
-        margin = self.axis_limit * 0.15
+        margin = self.axis_limit * 0.25
 
         # Setting the place of the label
         move_text = [0, 0]
         if x_value > 0:
-            move_text[0] = self.axis_limit * 0.01 + x_value
+            move_text[0] = self.axis_limit * 0.02 + x_value
         elif x_value < 0:
-            move_text[0] = -self.axis_limit * 0.08 + x_value
+            move_text[0] = -self.axis_limit * 0.12 + x_value
         if y_value > 0:
-            move_text[1] = self.axis_limit * 0.01 + y_value
+            move_text[1] = self.axis_limit * 0.02 + y_value
         elif y_value < 0:
-            move_text[1] = -self.axis_limit * 0.05 + y_value
+            move_text[1] = -self.axis_limit * 0.06 + y_value
 
         # Ploting
         self.ax1.quiver(0, 0, x_value, y_value, scale_units="xy", angles="xy", color=color, scale=1)
         self.ax1.annotate(label, xy=(x_value, y_value), xytext=move_text, color=color)
         self.ax1.axis([-self.axis_limit - margin, self.axis_limit + margin,
                        -self.axis_limit - margin, self.axis_limit + margin])
+        self.draw()
+
+    def delete_plot(self):
+        self.ax1.cla()
+        self.ax1.set_xticks([])
+        self.ax1.set_yticks([])
         self.draw()
 
 
